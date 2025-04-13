1edb5d6e823cab3f1a994f094eecf72efcf41722
hargg19 <sobaraharis@gmail.com>
2025-04-13 08:11:05 +0700

    Update pwm.c

--- pwm.c
+++ pwm.c
@@ -1,81 +1,53 @@
 #include "pwm.h"
 
-#define PWM_FREQ     50000  // 50kHz
-#define PWM_RES      1000   // 0-1000 = 0-100% resolution
+static uint16_t pwm_resolution;
 
-void PWM_Init(void) {
-    // 1. Enable Clocks
+// Helper untuk konversi channel ke register offset
+#define CH_TO_CCR(ch) (*(&TIM1->CCR1 + (ch)))
+
+void PWM_Init(uint32_t freq_hz, uint16_t resolution) {
+    pwm_resolution = resolution;
+    
+    /* 1. Enable Clock */
     RCC->APB2ENR |= RCC_APB2ENR_TIM1EN | RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN;
     
-    // 2. Configure GPIO (PA8/PA9/PA10 - Alternate Function Push-Pull)
+    /* 2. Configure GPIO Pins */
     GPIOA->CRH &= ~(GPIO_CRH_CNF8 | GPIO_CRH_CNF9 | GPIO_CRH_CNF10);
-    GPIOA->CRH |= (0xB << GPIO_CRH_CNF8_Pos) |  // CH1 (PA8)
-                  (0xB << GPIO_CRH_CNF9_Pos) |  // CH2 (PA9)
-                  (0xB << GPIO_CRH_CNF10_Pos);  // CH3 (PA10)
-    GPIOA->CRH |= (0x3 << GPIO_CRH_MODE8_Pos) | // 50MHz
-                  (0x3 << GPIO_CRH_MODE9_Pos) |
-                  (0x3 << GPIO_CRH_MODE10_Pos);
+    GPIOA->CRH |= (GPIO_CRH_CNF8_1 | GPIO_CRH_CNF9_1 | GPIO_CRH_CNF10_1); // AFPP
+    GPIOA->CRH |= (GPIO_CRH_MODE8_0 | GPIO_CRH_MODE8_1 | 
+                   GPIO_CRH_MODE9_0 | GPIO_CRH_MODE9_1 |
+                   GPIO_CRH_MODE10_0 | GPIO_CRH_MODE10_1); // 50MHz
     
-    // 3. Timer Base Configuration
-    uint16_t prescaler = (SystemCoreClock / (PWM_RES * PWM_FREQ)) - 1;
+    /* 3. Timer Base Configuration */
+    uint16_t prescaler = (SystemCoreClock / (resolution * freq_hz)) - 1;
     TIM1->PSC = prescaler;
-    TIM1->ARR = PWM_RES - 1;
-    TIM1->CR1 = TIM_CR1_ARPE; // Auto-reload preload enabled
+    TIM1->ARR = resolution - 1;
+    TIM1->CR1 = TIM_CR1_ARPE;
     
-    // 4. Output Compare Configuration
-    TIM1->CCMR1 = (6 << TIM_CCMR1_OC1M_Pos) | // PWM mode 1
-                  (6 << TIM_CCMR1_OC2M_Pos);
-    TIM1->CCMR2 = (6 << TIM_CCMR2_OC3M_Pos);
+    /* 4. Configure Output Compare Mode */
+    // Channel 1-3 PWM Mode 1 (110)
+    TIM1->CCMR1 = (6 << 4) | (6 << 12); // OC1M dan OC2M
+    TIM1->CCMR2 = (6 << 4);              // OC3M
     
-    TIM1->CCER |= TIM_CCER_CC1E | // Enable CH1
-                  TIM_CCER_CC2E | // Enable CH2
-                  TIM_CCER_CC3E;  // Enable CH3
+    /* 5. Enable Preload */
+    TIM1->CCMR1 |= TIM_CCMR1_OC1PE | TIM_CCMR1_OC2PE;
+    TIM1->CCMR2 |= TIM_CCMR2_OC3PE;
     
-    // 5. Enable PWM Outputs
-    TIM1->BDTR |= TIM_BDTR_MOE; // Main output enable
-    TIM1->CR1 |= TIM_CR1_CEN;   // Counter enable
+    /* 6. Enable Outputs */
+    TIM1->CCER |= TIM_CCER_CC1E | TIM_CCER_CC2E | TIM_CCER_CC3E;
+    TIM1->BDTR |= TIM_BDTR_MOE;
+    TIM1->CR1 |= TIM_CR1_CEN;
 }
 
-void PWM_SetDuty(PWM_Channel ch, uint16_t duty) {
-    if(duty > PWM_RES) duty = PWM_RES;
-    
-    switch(ch) {
-        case PWM_HEATER_T12:
-            TIM1->CCR1 = duty;
-            break;
-        case PWM_HEATER_HAIR:
-            TIM1->CCR2 = duty;
-            break;
-        case PWM_FAN:
-            TIM1->CCR3 = duty;
-            break;
-    }
+void PWM_SetDuty(PWM_Channel ch, float duty) {
+    duty = (duty < 0.0f) ? 0.0f : (duty > 100.0f) ? 100.0f : duty;
+    CH_TO_CCR(ch) = (uint16_t)((duty / 100.0f) * pwm_resolution);
 }
 
 void PWM_Enable(PWM_Channel ch) {
-    switch(ch) {
-        case PWM_HEATER_T12:
-            TIM1->CCER |= TIM_CCER_CC1E;
-            break;
-        case PWM_HEATER_HAIR:
-            TIM1->CCER |= TIM_CCER_CC2E;
-            break;
-        case PWM_FAN:
-            TIM1->CCER |= TIM_CCER_CC3E;
-            break;
-    }
+    TIM1->CCER |= (TIM_CCER_CC1E << ch);
 }
 
 void PWM_Disable(PWM_Channel ch) {
-    switch(ch) {
-        case PWM_HEATER_T12:
-            TIM1->CCER &= ~TIM_CCER_CC1E;
-            break;
-        case PWM_HEATER_HAIR:
-            TIM1->CCER &= ~TIM_CCER_CC2E;
-            break;
-        case PWM_FAN:
-            TIM1->CCER &= ~TIM_CCER_CC3E;
-            break;
-    }
+    TIM1->CCER &= ~(TIM_CCER_CC1E << ch);
 }
