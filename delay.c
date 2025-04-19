5b686a91e244e879602aadeb29900e4820af9d88
hargg19 <sobaraharis@gmail.com>
2025-04-19 15:43:00 +0700

    Update delay.c

--- delay.c
+++ delay.c
@@ -1,24 +1,33 @@
 #include "delay.h"
 #include "stm32f10x.h"
 
-// Variabel untuk menghitung nilai tick
-static uint32_t us_ticks;
+// Variabel untuk mencatat waktu berjalan dalam milidetik
+static volatile uint32_t millis = 0;
 
-// Inisialisasi SysTick untuk delay
-void delay_init(void) {
-    // Konfigurasi SysTick untuk menghasilkan tick setiap 1 us
-    us_ticks = SystemCoreClock / 1000000;
-
-    // Atur SysTick untuk interval maksimum
-    SysTick->LOAD = 0xFFFFFF; // Nilai maksimum
-    SysTick->VAL = 0;         // Reset nilai counter
-    SysTick->CTRL = SysTick_CTRL_ENABLE_Msk; // Aktifkan SysTick tanpa interrupt
+// Fungsi handler SysTick
+void SysTick_Handler(void) {
+    millis++; // Increment setiap 1 ms
 }
 
-// Delay dalam mikrodetik
+// Inisialisasi SysTick untuk delay dan waktu berjalan
+void delay_init(void) {
+    // Konfigurasi SysTick untuk menghasilkan interrupt setiap 1 ms
+    SysTick_Config(SystemCoreClock / 1000); // 1 ms tick
+
+    // Pastikan SysTick interrupt diaktifkan
+    NVIC_SetPriority(SysTick_IRQn, 0); // Prioritas tertinggi
+    NVIC_EnableIRQ(SysTick_IRQn);
+}
+
+// Fungsi untuk mendapatkan waktu berjalan dalam milidetik
+uint32_t delay_millis(void) {
+    return millis; // Kembalikan nilai waktu berjalan
+}
+
+// Fungsi delay dalam mikrodetik
 void delay_us(uint32_t us) {
     uint32_t start_tick = SysTick->VAL; // Ambil nilai awal dari SysTick
-    uint32_t ticks = us * us_ticks;
+    uint32_t ticks = us * (SystemCoreClock / 1000000); // Hitung jumlah tick untuk us
 
     // Tunggu hingga delay selesai
     while ((start_tick - SysTick->VAL) < ticks) {
@@ -29,7 +38,7 @@ void delay_us(uint32_t us) {
     }
 }
 
-// Delay dalam milidetik
+// Fungsi delay dalam milidetik
 void delay_ms(uint32_t ms) {
     for (uint32_t i = 0; i < ms; i++) {
         delay_us(1000); // 1 ms = 1000 us
