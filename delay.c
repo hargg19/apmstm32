c7af60379102b8011642ae6fa1639b141f351aeb
hargg19 <sobaraharis@gmail.com>
2025-04-19 15:24:50 +0700

    Update delay.c

--- delay.c
+++ delay.c
@@ -1,62 +1,37 @@
 #include "delay.h"
+#include "stm32f10x.h"
 
-static volatile uint32_t overflow_count = 0;
+// Variabel untuk menghitung nilai tick
+static uint32_t us_ticks;
 
-void TIM5_IRQHandler(void)
-{
-    if(TIM5->SR & TIM_SR_UIF)
-    {
-        TIM5->SR &= ~TIM_SR_UIF;
-        overflow_count++;
+// Inisialisasi SysTick untuk delay
+void delay_init(void) {
+    // Konfigurasi SysTick untuk menghasilkan tick setiap 1 us
+    us_ticks = SystemCoreClock / 1000000;
+
+    // Atur SysTick untuk interval maksimum
+    SysTick->LOAD = 0xFFFFFF; // Nilai maksimum
+    SysTick->VAL = 0;         // Reset nilai counter
+    SysTick->CTRL = SysTick_CTRL_ENABLE_Msk; // Aktifkan SysTick tanpa interrupt
+}
+
+// Delay dalam mikrodetik
+void delay_us(uint32_t us) {
+    uint32_t start_tick = SysTick->VAL; // Ambil nilai awal dari SysTick
+    uint32_t ticks = us * us_ticks;
+
+    // Tunggu hingga delay selesai
+    while ((start_tick - SysTick->VAL) < ticks) {
+        if (SysTick->VAL > start_tick) { // Jika counter SysTick melingkar
+            ticks -= start_tick + (0xFFFFFF - SysTick->VAL);
+            start_tick = 0xFFFFFF;
+        }
     }
 }
 
-void delay_init(void)
-{
-    // 1. Enable clock for TIM5
-    RCC->APB1ENR |= RCC_APB1ENR_TIM5EN;
-    
-    // 2. Configure timer for 1MHz (1us resolution)
-    // Assuming 72MHz APB1 clock (default)
-    TIM5->PSC = (SystemCoreClock / 1000000) - 1;
-    
-    // 3. Set auto-reload to maximum (16-bit)
-    TIM5->ARR = 0xFFFF;
-    
-    // 4. Enable overflow interrupt
-    TIM5->DIER |= TIM_DIER_UIE;
-    NVIC_EnableIRQ(TIM5_IRQn);
-    NVIC_SetPriority(TIM5_IRQn, 0);
-    
-    // 5. Start timer
-    TIM5->CR1 |= TIM_CR1_CEN;
-}
-
-void delay_us(uint32_t us)
-{
-    uint32_t start = delay_micros();
-    while((delay_micros() - start) < us);
-}
-
-void delay_ms(uint32_t ms)
-{
-    uint32_t start = delay_millis();
-    while((delay_millis() - start) < ms);
-}
-
-uint32_t delay_micros(void)
-{
-    uint32_t cnt, ovf;
-    
-    do {
-        ovf = overflow_count;
-        cnt = TIM5->CNT;
-    } while(ovf != overflow_count); // Handle race condition
-    
-    return (ovf << 16) + cnt;
-}
-
-uint32_t delay_millis(void)
-{
-    return delay_micros() / 1000;
+// Delay dalam milidetik
+void delay_ms(uint32_t ms) {
+    for (uint32_t i = 0; i < ms; i++) {
+        delay_us(1000); // 1 ms = 1000 us
+    }
 }
