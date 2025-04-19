e6444f873ae7cab05b54bf584c421f8bed3d2e2f
hargg19 <sobaraharis@gmail.com>
2025-04-19 15:37:46 +0700

    Update pwm.h

--- pwm.h
+++ pwm.h
@@ -1,18 +1,12 @@
-#ifndef PWM_H
-#define PWM_H
+#ifndef __PWM_H
+#define __PWM_H
 
-#include "stm32f10x.h"
+#include <stdint.h>
 
-// Definisi channel manual jika CMSIS tidak menyediakan
-typedef enum {
-    PWM_CH1 = 0x00,  // TIM_Channel_1 (PA8)
-    PWM_CH2 = 0x01,  // TIM_Channel_2 (PA9)
-    PWM_CH3 = 0x02   // TIM_Channel_3 (PA10)
-} PWM_Channel;
+void PWM_Init(void);
+void Gmos1_SetDuty(uint8_t duty);
+void Gmos2_SetDuty(uint8_t duty);
+void Gmos1_SetFreq(uint32_t freq);
+void Gmos2_SetFreq(uint32_t freq);
 
-void PWM_Init(uint32_t freq_hz, uint16_t resolution);
-void PWM_SetDuty(PWM_Channel ch, float duty);
-void PWM_Enable(PWM_Channel ch);
-void PWM_Disable(PWM_Channel ch);
-
-#endif // PWM_H
+#endif // __PWM_H
