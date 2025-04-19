cb62087575f40033e7c97dc002248f4fdff32b29
hargg19 <sobaraharis@gmail.com>
2025-04-19 22:34:47 +0700

    Update buttonsetpoint.c

--- buttonsetpoint.c
+++ buttonsetpoint.c
@@ -9,37 +9,32 @@ void ButtonSetpoint_Init(void) {
     // Aktifkan clock untuk GPIOA dan GPIOB
     RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN;
 
-    // Konfigurasi pin PA11 dan PA12 untuk setpoint1 sebagai input pull-down
-    GPIOA->CRH &= ~(GPIO_CRH_MODE11 | GPIO_CRH_CNF11 | GPIO_CRH_MODE12 | GPIO_CRH_CNF12);
-    GPIOA->CRH |= (GPIO_CRH_CNF11_1 | GPIO_CRH_CNF12_1); // CNF = 10 (Input pull-down)
-    GPIOA->ODR &= ~(GPIO_ODR_ODR11 | GPIO_ODR_ODR12);    // Pull-down enable
+    // Konfigurasi pin PA7 dan PA6 untuk setpoint1 sebagai input pull-up
+    GPIOA->CRL &= ~(GPIO_CRL_MODE7 | GPIO_CRL_CNF7 | GPIO_CRL_MODE6 | GPIO_CRL_CNF6);
+    GPIOA->CRL |= (GPIO_CRL_CNF7_1 | GPIO_CRL_CNF6_1); // CNF = 10 (Input pull-up)
+    GPIOA->ODR |= (GPIO_ODR_ODR7 | GPIO_ODR_ODR6);     // Pull-up enable
 
-    // Konfigurasi pin PA15 untuk setpoint2 sebagai input pull-down
-    GPIOA->CRH &= ~(GPIO_CRH_MODE15 | GPIO_CRH_CNF15);
-    GPIOA->CRH |= GPIO_CRH_CNF15_1; // CNF = 10 (Input pull-down)
-    GPIOA->ODR &= ~GPIO_ODR_ODR15;  // Pull-down enable
-
-    // Konfigurasi pin PB3 untuk setpoint2 sebagai input pull-down
-    GPIOB->CRL &= ~(GPIO_CRL_MODE3 | GPIO_CRL_CNF3);
-    GPIOB->CRL |= GPIO_CRL_CNF3_1; // CNF = 10 (Input pull-down)
-    GPIOB->ODR &= ~GPIO_ODR_ODR3;  // Pull-down enable
+    // Konfigurasi pin PA5 dan PA4 untuk setpoint2 sebagai input pull-up
+    GPIOA->CRL &= ~(GPIO_CRL_MODE5 | GPIO_CRL_CNF5 | GPIO_CRL_MODE4 | GPIO_CRL_CNF4);
+    GPIOA->CRL |= (GPIO_CRL_CNF5_1 | GPIO_CRL_CNF4_1); // CNF = 10 (Input pull-up)
+    GPIOA->ODR |= (GPIO_ODR_ODR5 | GPIO_ODR_ODR4);     // Pull-up enable
 }
 
 // Fungsi untuk memperbarui nilai setpoint berdasarkan tombol yang ditekan
 void ButtonSetpoint_Update(void) {
     // Periksa tombol untuk setpoint1
-    if (GPIOA->IDR & GPIO_IDR_IDR11) {
+    if (!(GPIOA->IDR & GPIO_IDR_IDR7)) { // Tombol PA7 ditekan (logika pull-up)
         setpoint1 += 1; // Tambah nilai setpoint1
     }
-    if (GPIOA->IDR & GPIO_IDR_IDR12) {
+    if (!(GPIOA->IDR & GPIO_IDR_IDR6)) { // Tombol PA6 ditekan (logika pull-up)
         setpoint1 -= 1; // Kurangi nilai setpoint1
     }
 
     // Periksa tombol untuk setpoint2
-    if (GPIOA->IDR & GPIO_IDR_IDR15) {
+    if (!(GPIOA->IDR & GPIO_IDR_IDR5)) { // Tombol PA5 ditekan (logika pull-up)
         setpoint2 += 1; // Tambah nilai setpoint2
     }
-    if (GPIOB->IDR & GPIO_IDR_IDR3) {
+    if (!(GPIOA->IDR & GPIO_IDR_IDR4)) { // Tombol PA4 ditekan (logika pull-up)
         setpoint2 -= 1; // Kurangi nilai setpoint2
     }
 }
@@ -56,10 +51,10 @@ int ButtonSetpoint_GetSetpoint2(void) {
 
 // Fungsi untuk memeriksa apakah tombol T12 ditekan
 int ButtonSetpoint_T12Pressed(void) {
-    return (GPIOA->IDR & GPIO_IDR_IDR11) || (GPIOA->IDR & GPIO_IDR_IDR12);
+    return (!(GPIOA->IDR & GPIO_IDR_IDR7)) || (!(GPIOA->IDR & GPIO_IDR_IDR6)); // Logika pull-up
 }
 
 // Fungsi untuk memeriksa apakah tombol JBC ditekan
 int ButtonSetpoint_JBCPressed(void) {
-    return (GPIOA->IDR & GPIO_IDR_IDR15) || (GPIOB->IDR & GPIO_IDR_IDR3);
+    return (!(GPIOA->IDR & GPIO_IDR_IDR5)) || (!(GPIOA->IDR & GPIO_IDR_IDR4)); // Logika pull-up
 }
