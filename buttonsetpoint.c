16d5a82e43fd4c8d77b3837cfe0d3c044d5ed3ae
hargg19 <sobaraharis@gmail.com>
2025-04-19 20:51:29 +0700

    Update buttonsetpoint.c

--- buttonsetpoint.c
+++ buttonsetpoint.c
@@ -1,4 +1,4 @@
-#include "buttonsetpoint.h"
+#include "stm32f10x.h"
 
 // Variabel untuk menyimpan nilai setpoint
 static volatile int setpoint1 = 300; // Nilai default 300
@@ -7,39 +7,39 @@ static volatile int setpoint2 = 300; // Nilai default 300
 // Inisialisasi pin untuk tombol
 void ButtonSetpoint_Init(void) {
     // Aktifkan clock untuk GPIOA dan GPIOB
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB, ENABLE);
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN;
 
-    // Konfigurasi pin PA11 dan PA12 untuk setpoint1
-    GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11 | GPIO_Pin_12;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; // Input dengan pull-down
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    // Konfigurasi pin PA11 dan PA12 untuk setpoint1 sebagai input pull-down
+    GPIOA->CRH &= ~(GPIO_CRH_MODE11 | GPIO_CRH_CNF11 | GPIO_CRH_MODE12 | GPIO_CRH_CNF12);
+    GPIOA->CRH |= (GPIO_CRH_CNF11_1 | GPIO_CRH_CNF12_1); // CNF = 10 (Input pull-down)
+    GPIOA->ODR &= ~(GPIO_ODR_ODR11 | GPIO_ODR_ODR12);    // Pull-down enable
 
-    // Konfigurasi pin PA15 untuk setpoint2
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    // Konfigurasi pin PA15 untuk setpoint2 sebagai input pull-down
+    GPIOA->CRH &= ~(GPIO_CRH_MODE15 | GPIO_CRH_CNF15);
+    GPIOA->CRH |= GPIO_CRH_CNF15_1; // CNF = 10 (Input pull-down)
+    GPIOA->ODR &= ~GPIO_ODR_ODR15;  // Pull-down enable
 
-    // Konfigurasi pin PB3 untuk setpoint2
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-    GPIO_Init(GPIOB, &GPIO_InitStructure);
+    // Konfigurasi pin PB3 untuk setpoint2 sebagai input pull-down
+    GPIOB->CRL &= ~(GPIO_CRL_MODE3 | GPIO_CRL_CNF3);
+    GPIOB->CRL |= GPIO_CRL_CNF3_1; // CNF = 10 (Input pull-down)
+    GPIOB->ODR &= ~GPIO_ODR_ODR3;  // Pull-down enable
 }
 
 // Fungsi untuk memperbarui nilai setpoint berdasarkan tombol yang ditekan
 void ButtonSetpoint_Update(void) {
     // Periksa tombol untuk setpoint1
-    if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_11)) {
+    if (GPIOA->IDR & GPIO_IDR_IDR11) {
         setpoint1 += 1; // Tambah nilai setpoint1
     }
-    if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_12)) {
+    if (GPIOA->IDR & GPIO_IDR_IDR12) {
         setpoint1 -= 1; // Kurangi nilai setpoint1
     }
 
     // Periksa tombol untuk setpoint2
-    if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_15)) {
+    if (GPIOA->IDR & GPIO_IDR_IDR15) {
         setpoint2 += 1; // Tambah nilai setpoint2
     }
-    if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_3)) {
+    if (GPIOB->IDR & GPIO_IDR_IDR3) {
         setpoint2 -= 1; // Kurangi nilai setpoint2
     }
 }
